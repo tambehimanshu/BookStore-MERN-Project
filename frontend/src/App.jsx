30f9b54ef61a4f06e8636c8bdc98082bdc6d92d1
tambehimanshu <tambehimanshu18@gmail.com>
2025-02-27 22:35:56 +0530

    hedersection with responsiveness

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Baner from './components/Baner'
 
 function App() {
   return (
     <>
     
     <Navbar/>
+    <Baner/>
     </>
   )
 }
